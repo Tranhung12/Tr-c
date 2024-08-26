d55e5e5a2b3ca6dad332ae5e36ef9da90dd0d0c2
Tranhung12 <86721468+Tranhung12@users.noreply.github.com>
2024-08-26 20:37:11 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,15 @@
+const CONFIG = {
+    introTitle: 'Trúc ơiii',
+    introDesc: `em có biết anh thương em nhiều lắm hong ?`,
+    btnIntro: 'hihi',
+    title: 'em có thương yêu anh honggg ?',
+    desc: 'đừng có hong đó nhaa ',
+    btnYes: 'có , được chưaaa -.-   ',
+    btnNo: 'Không, anh à !',
+    question:'biết sao anh thương em honggg ?',
+    btnReply: 'có xạo quá không vậy',
+    reply: 'tại Trúc dễ thương lắm lắm lunnn á  ',
+    messDesc: 'thật lòng mà nếu có em thì anh sẽ làm mọi cách để em yêu anh hoài luôn hehee',
+    btnAccept: 'thật 100%, yêu anh đi rồi anh là của em từ nay về sau',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
